559c0e4fef7cc632d5e1f5f62802f481ceedec9e
kaylavera99 <kaylavd99@gmail.com>
2024-05-23 15:19:24 -0400

    Update helloworld.js
    Updated the JS file

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Welcome to my Web Application!\n')
+   response.end('Welcome to my Web Application!\n - Kayla Duffy | Week 3')
 }).listen(3333)
 
 // Console will print the message
